a97fb95886ff9086e68bbd907b89149ca14669c3
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-02-07 21:41:09 -0100

    Created userschema, added router to create new user. hash password implemented

--- server/app.js
+++ server/app.js
@@ -1,20 +1,41 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
 
-var app = express();
+//Set up cors
+const cors = require('cors')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//Set up DB connection
+const mongoose = require('mongoose')
+mongoose
+  .connect(
+    'mongodb+srv://ratz:banana69@cluster0.4j52b.mongodb.net/messenger_app?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log('Connected with DB successfully')
+  })
+  .catch((err) => {
+    console.log(err.message)
+  })
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var app = express()
 
-module.exports = app;
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+module.exports = app
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+const Users = require('../models/users')
+var router = express.Router()
+const bcrypt = require('bcrypt')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/', async (req, res, next) => {
+  const { userName, password, emoji } = req.body
+  console.log(req.body)
+  try {
+    const newUser = new Users({
+      userName,
+      password: bcrypt.hashSync(password, 10),
+      emoji,
+    })
+    await newUser.save()
 
-module.exports = router;
+    return res.status(201).json('Created new user successfully!')
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json('Cant create user')
+  }
+})
+
+module.exports = router
