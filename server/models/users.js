35be2a2c458cb7f43a493fe8d691527af13b94a3
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-02-11 18:07:07 -0100

    Added login endpoint, and set token to user

--- server/models/users.js
+++ server/models/users.js
@@ -6,6 +6,7 @@ const Users = mongoose.model(
     username: {
       type: String,
       required: true,
+      unique: true,
     },
     password: {
       type: String,
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,9 @@
 var express = require('express')
-const Users = require('../models/users')
 var router = express.Router()
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const Users = require('../models/users')
 
 router.post('/', async (req, res) => {
   const { username, password, emoji } = req.body
@@ -18,6 +20,32 @@ router.post('/', async (req, res) => {
   } catch (error) {
     console.log(error)
     return res.status(500).json('Cant create user')
+  }
+})
+
+router.post('/login', async (req, res) => {
+  const { username, password, emoji } = req.body
+
+  try {
+    const user = await Users.findOne({ username })
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(400).json({ message: 'Login failed' })
+    }
+
+    const token = jwt.sign(
+      {
+        username: user.username,
+      },
+      'ratzSecret'
+    )
+
+    return res.status(200).json({
+      message: `${user.username} logged in`,
+      emoji,
+      token,
+    })
+  } catch (error) {
+    console.log(error)
   }
 })
 
