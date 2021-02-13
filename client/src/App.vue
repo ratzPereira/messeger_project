7fb8a44a9fff1a69570496cdedc664b3eaebffa4
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-02-13 15:44:24 -0100

    Added styles to Home page, emojis, register form..

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,98 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container">
+      <div class="header">
+        <button class="btn-logout">Logout</button>
+        <h1>Welcome to Twinder</h1>
+      </div>
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+body {
+  background-color: #cde;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialised;
+  -moz-osx-font-smoothing: grayscale;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.container {
+  background-color: #fff;
+  max-width: 400px;
+  margin: 0 auto 10px;
+  min-height: 600px;
+  border-radius: 5px;
+}
+
+.header {
+  position: relative;
+  height: 40px;
+  background-color: #635aaa;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+
+.btn-logout {
+  cursor: pointer;
+  position: absolute;
+  right: 10px;
+  top: 5px;
+  background-color: #150a75;
+  color: #fff;
+  border: 0;
+  border-radius: 3px;
+  padding: 5px;
+  margin: 5px;
+}
+
+h1 {
+  font-size: 18px;
+  line-height: 20px;
+  padding-top: 10px;
+  color: #eee;
+  text-align: center;
+}
+
+.field-area {
+  padding: 5px 10px;
+}
+
+label {
+  display: none;
+}
+
+input {
+  display: block;
+  width: 100%;
+  border: 1px solid #999;
+  padding: 10px;
+  border-radius: 3px;
+}
+
+.error-message {
+  font-size: 11px;
+  color: red;
+}
+
+.button-register {
+  cursor: pointer;
+  background-color: #66f;
+  border: none;
+  border-radius: 3px;
+  padding: 15px;
+  width: 100%;
+  color: #eee;
+  font-size: 18px;
 }
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Chat" src="../assets/logo.png" class="logo" />
+    <div class="formLogin">
+      <form @submit.prevent="onSubmit">
+        <div class="errorMessage" v-if="errorMessage">{{ errorMessage }}</div>
+        <div class="field-area">
+          <label for="input-username">Name:</label>
+          <input
+            type="text"
+            :class="{ error: rules.username }"
+            id="input-username"
+            v-model="form.username"
+            placeholder="Insert your name"
+          />
+          <span class="error-message" v-show="rules.username">
+            {{ rules.username }}
+          </span>
+        </div>
+        <div class="field-area">
+          <label for="input-password">Password:</label>
+          <input
+            type="password"
+            :class="{ error: rules.password }"
+            id="input-password"
+            v-model="form.password"
+            placeholder="Insert your password"
+          />
+          <span class="error-message" v-show="rules.password">
+            {{ rules.password }}
+          </span>
+        </div>
+
+        <div class="field-area">
+          <p class="label-emoji">Choose your emoji: {{ form.emoji }}</p>
+          <div class="emoji-list" :class="{ error: rules.emoji }">
+            <span
+              class="emoji"
+              :class="{ selected: emoji == form.emoji }"
+              v-for="emoji in emojis"
+              :key="emoji"
+              @click="form.emoji = emoji"
+            >
+              {{ emoji }}
+            </span>
+          </div>
+          <span class="error-message" v-show="rules.emoji">
+            {{ rules.emoji }}
+          </span>
+        </div>
+
+        <div class="field-area">
+          <button class="button-register" type="submit">Register</button>
+        </div>
+      </form>
+      <p class="link-register">
+        <router-link to="/login">Already registed</router-link>
+      </p>
+    </div>
   </div>
 </template>
 
@@ -8,7 +64,100 @@
 // @ is an alias to /src
 
 export default {
-  name: 'Home',
+  name: 'home',
   components: {},
+
+  data: () => ({
+    emojis: [
+      '😀',
+      '😁',
+      '😊',
+      '😇',
+      '🥰',
+      '🤩',
+      '😚',
+      '🤪',
+      '🤑',
+      '🤭',
+      '🤐',
+      '😑',
+      '😒',
+      '😴',
+      '🤧',
+      '🤓',
+      '🥺',
+      '😱',
+      '🤬',
+      '💩',
+      '👀',
+      '🙅‍♂️',
+      '🤷‍♂️',
+      '👩‍🚀',
+    ],
+    errorMessage: null,
+    form: {
+      username: null,
+      password: null,
+      emoji: null,
+    },
+    rules: {
+      username: null,
+      password: null,
+      emoji: null,
+    },
+  }),
+  methods: {
+    onSubmit() {},
+  },
 }
 </script>
+
+<style lang="scss">
+.logo {
+  width: 60%;
+  margin: 10px 20%;
+}
+
+.emoji-list {
+  border: 1px solid #999;
+  border-radius: 3px;
+  font-size: 26px;
+  line-height: 35px;
+  display: grid;
+  grid-template-columns: 10% 10% 10% 10% 10% 10% 10%;
+  justify-content: space-between;
+  padding: 5px;
+}
+.emoji-list.error {
+  border: 1px solid #999;
+}
+
+.emoji {
+  text-align: center;
+  width: 40px;
+  height: 40px;
+  padding: 2px;
+  cursor: pointer;
+}
+
+.emoji:hover {
+  border: 1px solid cyan;
+  border-radius: 50%;
+  background-color: cyan;
+}
+
+.label-emoji {
+  color: #999;
+  padding-bottom: 5px;
+}
+
+.link-register {
+  text-align: center;
+}
+
+a {
+  display: block;
+  text-decoration: none;
+  padding: 10px;
+}
+</style>
